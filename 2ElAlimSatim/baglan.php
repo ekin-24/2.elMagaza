02fda9d9e69befd642eabc12ae63bc39063d0197
Ekin Çinar <34ekin24@gmail.com>
2026-05-24 10:12:25 +0300

    Veritabani baglantisi Kubernetes icin guncellendi

--- 2ElAlimSatim/baglan.php
+++ 2ElAlimSatim/baglan.php
@@ -1,8 +1,9 @@
 <?php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=2elMagaza;charset=utf8", "root", "");
+    $db = new PDO("mysql:host=mysql-service;dbname=2elMagaza;charset=utf8", "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Bağlantı hatası: " . $e->getMessage();
     die();
-} 
+}
+?>
